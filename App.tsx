25660151dcccd501429a9e9e63ea88cad7700313
Luiz Rioja <lulyrioja@gmail.com>
2021-11-13 15:35:45 -0300

    :alien: Configuring Sentry in the Application

--- App.tsx
+++ App.tsx
@@ -7,8 +7,17 @@ import SplashScreen from "react-native-splash-screen";
 
 import { Home } from "./src/pages/Home";
 
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: "https://38a829e0e647482fbe13eb655fc61ae7@o959685.ingest.sentry.io/6062673",
+  tracesSampleRate: 1.0,
+});
+
 function App() {
   useEffect(() => {
+    throw new Error("My first Sentry error!");
+
     codePush.sync({
       installMode: codePush.InstallMode.IMMEDIATE,
     });
